af7b3a088ebecedf5b2bf64c99c3723b525e6172
kittinan-k <kittinan-k@kyocera.co.th>
2024-02-26 10:39:47 +0700

    ok

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   apps: [
     {
-      name: "rgas-node",
+      name: "attendance-report-node",
       script: "./index.js", // your script
       watch: true,
       args: "start",
       env: {
         NODE_ENV: "production",
-        PORT: 4051,
-        DATABASE: "mongodb://10.200.90.152:27017/rgas",
+        PORT: 4058,
+        DATABASE: "mongodb://10.200.90.152:27017/attendance-report",
       },
     },
   ],
